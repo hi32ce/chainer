2e9a81846383192b0d21089a549fe18074fc565f
hi32ce <hi32ce_2@hotmail.com>
2018-05-24 06:29:09 +0000

    split predict.

--- examples/imagenet/resnet50.py
+++ examples/imagenet/resnet50.py
@@ -96,6 +96,13 @@ class ResNet50(chainer.Chain):
             self.fc = L.Linear(2048, 1000)
 
     def __call__(self, x, t):
+        h = self.predict(x)
+
+        loss = F.softmax_cross_entropy(h, t)
+        chainer.report({'loss': loss, 'accuracy': F.accuracy(h, t)}, self)
+        return loss
+
+    def predict(self, x): 
         h = self.bn1(self.conv1(x))
         h = F.max_pooling_2d(F.relu(h), 3, stride=2)
         h = self.res2(h)
@@ -104,11 +111,7 @@ class ResNet50(chainer.Chain):
         h = self.res5(h)
         h = F.average_pooling_2d(h, 7, stride=1)
         h = self.fc(h)
-
-        loss = F.softmax_cross_entropy(h, t)
-        chainer.report({'loss': loss, 'accuracy': F.accuracy(h, t)}, self)
-        return loss
-
+        return h
 
 class ResNeXt50(ResNet50):
 
